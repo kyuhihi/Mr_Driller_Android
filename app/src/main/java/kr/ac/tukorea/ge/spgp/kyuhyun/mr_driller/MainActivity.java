30933ca6917709282cf34630aa6829fcaacd3d9b
kkh <gimgyugyu96@gmail.com>
2024-04-03 00:14:05 +0900

    Build Project

---
+++ app/src/main/java/kr/ac/tukorea/ge/spgp/kyuhyun/mr_driller/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.tukorea.ge.spgp.kyuhyun.mr_driller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
